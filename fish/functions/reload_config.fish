4fb9a1b82be921fd176d2bb4b856bc1c9cfb395c
Marek Mateja <matejowy@gmail.com>
2017-03-11 13:06:36 +0100

    Remove abbreviations cleaning from reload_config function.

--- fish/functions/reload_config.fish
+++ fish/functions/reload_config.fish
@@ -2,7 +2,6 @@ function reload_config --description 'Reload configuration from repository'
 	echo 'Reloading configuration...'
 
 	set_general_config
-	set --erase fish_user_abbreviations
 	set_git_aliases
 
 	echo 'Done.'
---
+++ fish/functions/remove_abbreviations.fish
@@ -0,0 +1,5 @@
+function remove_abbreviations
+	echo 'Removing set abbreviations...'
+
+	set --erase fish_user_abbreviations
+end
