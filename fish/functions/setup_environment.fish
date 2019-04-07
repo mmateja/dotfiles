8fdc756b85053540a6427ada2014721dd109c78b
Marek Mateja <matejowy@gmail.com>
2019-04-07 21:10:29 +0200

    Add directory disk space usage alias

--- fish/functions/setup_environment.fish
+++ fish/functions/setup_environment.fish
@@ -2,6 +2,7 @@ function setup_environment --description 'Load configuration from the repository
 	echo 'Loading configuration...'
 
 	set_general_config
+	set_general_purpose_aliases
 	set_git_aliases
 	set_vagrant_aliases
 	set_navigational_aliases
