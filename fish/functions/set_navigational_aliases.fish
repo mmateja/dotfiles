1666085376022752eafcde2b34e4eb4302f8eac4
Marek Mateja <matejowy@gmail.com>
2019-04-07 15:09:29 +0200

    Add navigational aliases to repo

---
+++ fish/functions/set_navigational_aliases.fish
@@ -0,0 +1,11 @@
+function set_navigational_aliases --description 'Set navigational aliases'
+	echo 'Setting navigational aliases...'
+
+	abbr cdc 'cd ~/dev/ns/ns-chef-repo'
+	abbr cdh 'cd ~/dev/ns/hydra'
+	abbr cdm 'cd ~/dev/ns/mjollnir'
+	abbr cds 'cd ~/dev/ns/socialguide'
+	abbr cdt 'cd ~/dev/ns/ns-terraform'
+	abbr cdw 'cd ~/dev/ns/ns-web'
+end
+
--- fish/functions/setup_environment.fish
+++ fish/functions/setup_environment.fish
@@ -4,6 +4,7 @@ function setup_environment --description 'Load configuration from the repository
 	set_general_config
 	set_git_aliases
 	set_vagrant_aliases
+	set_navigational_aliases
 	fish_update_completions
 
 	echo 'Done.'
