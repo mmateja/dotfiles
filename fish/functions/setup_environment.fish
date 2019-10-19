e7d1a1a91dd3cef1062212c54606b04647cc03c8
Marek Mateja <matejowy@gmail.com>
2019-10-19 10:07:57 +0200

    Add Docker aliases

--- fish/functions/setup_environment.fish
+++ fish/functions/setup_environment.fish
@@ -5,6 +5,7 @@ function setup_environment --description 'Load configuration from the repository
 	set_general_purpose_aliases
 	set_git_aliases
 	set_vagrant_aliases
+	set_docker_aliases
 	set_navigational_aliases
 	fish_update_completions
 
