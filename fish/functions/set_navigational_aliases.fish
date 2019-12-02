c95eab66a0ab781d45b7caff3f68e29664b752fa
Marek Mateja <matejowy@gmail.com>
2019-12-02 11:44:18 +0100

    Add alias to Icarus repo

--- fish/functions/set_navigational_aliases.fish
+++ fish/functions/set_navigational_aliases.fish
@@ -3,6 +3,7 @@ function set_navigational_aliases --description 'Set navigational aliases'
 
 	abbr cdc 'cd ~/dev/ns/ns-chef-repo'
 	abbr cdh 'cd ~/dev/ns/hydra'
+	abbr cdi 'cd ~/dev/ns/icarus'
 	abbr cdm 'cd ~/dev/ns/mjollnir'
 	abbr cds 'cd ~/dev/ns/socialguide'
 	abbr cdt 'cd ~/dev/ns/ns-terraform'
