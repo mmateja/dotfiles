fccc6c9bc3bec38216a473f02d3b966aee2d5f53
Marek Mateja <matejowy@gmail.com>
2026-06-10 09:09:08 +0200

    Fix `clean-branches` function

--- fish/functions/clean-branches.fish
+++ fish/functions/clean-branches.fish
@@ -1,5 +1,8 @@
-function clean-branchesi -d "Select and remove local branches"
-	git branch -v | grep '[gone]' | awk '{print $1}' | fzf -m --header "Select branches with TAB and hit ENTER to
-	remove them" | xargs git branch -D
+function clean-branches -d "Select and remove local branches"
+	git branch -v | \
+		grep '[gone]' | \
+		awk '{print $1}' | \
+		fzf -m --header "Mark branches with TAB and hit ENTER to remove selected" | \
+		xargs git branch -D
 end
 
